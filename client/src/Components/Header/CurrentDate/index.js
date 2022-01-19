9cacbdb0b7293806926c5e87f1f0688ecc6d00f2
Luis0lo <58235960+Luis0lo@users.noreply.github.com>
2022-01-19 23:16:53 +0000

    dynamic date

--- client/src/Components/Header/CurrentDate/index.js
+++ client/src/Components/Header/CurrentDate/index.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import "./styles.css";
+import './styles.css';
 
 const CurrentDate = () => {
+  const date = new Date();
+  const day = date.getDate();
+  const year = date.getFullYear();
+  const month = date.toLocaleString('default', { month: 'long' });
+  let ordinal = '';
+
+  if (day === 1 || day === 21 || day === 31) {
+    ordinal = 'st';
+  } else if (day === 2 || day === 22) {
+    ordinal = 'nd';
+  } else if (day === 3) {
+    ordinal = 'rd';
+  } else {
+    ordinal = 'th';
+  }
+
+  const currentDate = `${day}${ordinal} ${month} ${year}`;
+
   return (
     <div className="Current_Date_Container">
-      <div className="Current_Date">17th January 2022</div>
-      <div ><em className="Current_Week">Week 9</em> </div>
+      <div className="Current_Date">{currentDate}</div>
+      <div>
+        <em className="Current_Week">Week 9</em>
+      </div>
     </div>
   );
 };
