6d4443bf2f548af08249690c479816226faa6bd6
Rikiah <rik_28@hotmail.co.uk>
2022-01-20 13:40:30 +0000

    File structuring

---
+++ client/src/Components/App/LoginComponent/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Input from './Input';
+import NewUserlink from './NewUserlink';
+import Title from './Title';
+
+
+export const LoginComponent = () => {
+  return <div>
+  <Input />
+  <NewUserlink />
+  <Title />
+
+  </div>;
+};
