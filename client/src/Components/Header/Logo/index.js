7061e0f33a2688bc79bbe08079ecda9824e16ed0
Luis0lo <58235960+Luis0lo@users.noreply.github.com>
2022-01-20 09:51:33 +0000

    header to deploy

--- client/src/Components/Header/Logo/index.js
+++ client/src/Components/Header/Logo/index.js
@@ -6,11 +6,7 @@ const Logo = () => {
   return (
     <div className="Logo">
       <div className='images'>
-        <img
-          className="Image_Logo"
-          src="https://d33wubrfki0l68.cloudfront.net/e6fddcbea146f91d2f3c160f7d56a9391a4740b0/4e758/static/logo-51c754388b198e5bbb0d08a971ebbfa2.png"
-          alt=""
-        />
+        
         <img className="Image_Logo2" src={logoImg} alt="" />
       </div>
       <div className='Logo_Name'>
